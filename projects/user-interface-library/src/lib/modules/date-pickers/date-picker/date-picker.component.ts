9a1fc2a1cea6d154d61465c36076218f44e25c0b
LukeBarrick <me@lukebarrick.co.uk>
2024-08-14 20:37:37 +0100

    date picker date and applied focus outline to selects

--- projects/user-interface-library/src/lib/modules/date-pickers/date-picker/date-picker.component.ts
+++ projects/user-interface-library/src/lib/modules/date-pickers/date-picker/date-picker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UUIDService } from '../../../core/services/UUID.service';
 
 @Component({
   selector: 'uilibrary-date-picker',
@@ -6,19 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-picker.component.css']
 })
 export class DatePickerComponent implements OnInit {
-
+  public id = this.UUID.generate();
+  public value: string | undefined = undefined;
+  
   isOpen: boolean = false;
 
-  currentDay: number = 0;
-  currentMonth: number = 0;
-  currentYear: number = 0;
+  currentDay!: number;
+  currentMonth!: number;
+  currentYear!: number;
+  daysInMonth: number[] = [];
+  weeksInMonth: any[][] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private UUID: UUIDService) {
+    const today = new Date();
+    this.currentDay = today.getDay();
+    this.currentMonth = today.getMonth();
+    this.currentYear = today.getFullYear();
+    this.generateCalendar(this.currentMonth, this.currentYear);
   }
 
-  toggleOpen(): void {
+  ngOnInit() {
+    console.log('Date picker is work in progress, please do not use.')
+  }
+
+  generateCalendar(month: number, year: number) {
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const calendarDays: any[] = [];
+
+    //Fill empty days
+    for(let i = 0; i < firstDayOfMonth; i++) {
+      calendarDays.push(null);
+    }
+
+    //Push days into array
+    for(let day = 0; day < daysInMonth; day++) {
+      calendarDays.push(day)
+    }
+
+    //Reset array, iterate and splice grouping by week
+    this.weeksInMonth = [];
+    while(calendarDays.length) {
+      this.weeksInMonth.push(calendarDays.splice(0, 7))
+    }
+  }
+
+  onFocus(event: any): void {
     this.isOpen = !this.isOpen;
   }
 }
