fddd614d85272c7e24ede5b9d547ea3790c30dd1
LukeBarrick <me@lukebarrick.co.uk>
2025-03-24 23:48:02 +0000

    Updated showcase with example

--- projects/uilibrary/src/lib/showcase/showcase.component.ts
+++ projects/uilibrary/src/lib/showcase/showcase.component.ts
@@ -78,8 +78,9 @@ export class ShowcaseComponent implements OnInit {
 
   textInputs2 = new FormGroup({
     textInput21: new FormControl('',[Validators.required]),
-    textInput22: new FormControl({value: '', disabled: true},[Validators.required]),
-    textInput23: new FormControl({value: 'Nancy', disabled: true},[Validators.required]),
+    textInput22: new FormControl('Nancy',[Validators.required]),
+    textInput23: new FormControl({value: '', disabled: true},[Validators.required]),
+    textInput24: new FormControl({value: 'Nancy', disabled: true},[Validators.required]),
   });
 
   textAreas = new FormGroup({
