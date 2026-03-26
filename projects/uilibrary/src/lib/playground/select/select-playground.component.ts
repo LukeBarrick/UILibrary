42f2fe4ba5d5f0d8583a01c5aeddc4b3e75cf540
Luke <me@lukebarrick.co.uk>
2026-03-26 23:17:55 +0000

    feat | Added playground component showcase for Select component, both default template & custom templates

--- projects/uilibrary/src/lib/playground/select/select-playground.component.ts
+++ projects/uilibrary/src/lib/playground/select/select-playground.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'uilibrary-select-playground',
@@ -20,12 +20,42 @@ export class SelectPlaygroundComponent implements OnInit {
     ]
 
     selectModel = { id: 1, value: 'Item 1' };
+    basicSelectBinding = this.items[0];
+    basicSelectDisabled: Item | undefined;
+    basicSelectIsDisabled: boolean = true;
+    customTemplateBindingDisabled: Item | undefined;
+    customTemplateBindingIsDisabled: boolean = true;
+    basicSelectError: Item | undefined = undefined;
     clearable = this.items[0];
+
+    formControlSelectForm: FormGroup = this.fb.group({
+        formControlSelect: [],
+        formControlSelectWithValue: [this.items[0]],
+        formControlSelectDisabled: [{ value: '', disabled: true }],
+        formControlSelectError: [undefined, Validators.required],
+        formControlSelectUntouched: [undefined, Validators.required]
+    });
+
+    customTemplateFormControlSelectForm: FormGroup = this.fb.group({
+        customFormControlSelect: [],
+        customFormControlSelectWithValue: [this.items[0]],
+        customFormControlSelectDisabled: [{ value: '', disabled: true }],
+        customFormControlSelectError: [undefined, Validators.required],
+        customFormControlSelectUntouched: [undefined, Validators.required]
+    });
+
+    formControlSelectErrorForm: FormGroup = this.fb.group({
+        formControlSelectError: [undefined, Validators.required]
+    });
 
     constructor() { }
 
     ngOnInit() {
-        // this.clearable = { id: 1, value: "Item 1"}
+        const errorFormControl = this.formControlSelectForm.get('formControlSelectError');
+        const customFormControl = this.customTemplateFormControlSelectForm.get('customFormControlSelectError');
+
+        customFormControl?.markAsTouched();
+        errorFormControl?.markAsTouched();
     }
 
     stringify(value: string) {
@@ -34,6 +64,38 @@ export class SelectPlaygroundComponent implements OnInit {
 
     exampleCompareFn(item: any, selected: any) {
         return item.id === selected.id;
+    }
+
+    toggleBindingDisabled() {
+        this.basicSelectIsDisabled = !this.basicSelectIsDisabled;
+    }
+
+    toggleCustomTemplateBindingDisabled() {
+        this.customTemplateBindingIsDisabled = !this.customTemplateBindingIsDisabled;
+    }
+
+    toggleCustomDisabledFormControl() {
+       const formControl = this.customTemplateFormControlSelectForm.get('customFormControlSelectDisabled');
+
+       if(!formControl) return;
+
+       if(formControl.disabled) {
+        formControl.enable();
+       } else {
+        formControl.disable();
+       }
+    }
+
+     toggleDisabledFormControl() {
+       const formControl = this.formControlSelectForm.get('formControlSelectDisabled');
+
+       if(!formControl) return;
+
+       if(formControl.disabled) {
+        formControl.enable();
+       } else {
+        formControl.disable();
+       }
     }
 }
 
