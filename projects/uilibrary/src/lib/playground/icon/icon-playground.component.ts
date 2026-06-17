6d02bc379f9d12a53464260065879ce3c9ceac88
LukeBarrick <61234214+LukeBarrick@users.noreply.github.com>
2026-06-17 01:01:28 +0100

    Update icon-playground.component.ts

--- projects/uilibrary/src/lib/playground/icon/icon-playground.component.ts
+++ projects/uilibrary/src/lib/playground/icon/icon-playground.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IconConstants } from '../../core/constants/icon.constants';
 
 @Component({
     selector: 'uilibrary-icon-playground',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
     standalone: false
 })
 
-export class IconPlaygroundComponent implements OnInit {
-    constructor() { }
+export class IconPlaygroundComponent {
 
-    ngOnInit() { }
+  availableIcons = Object.values(IconConstants);
 }
