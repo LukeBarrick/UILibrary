235e55beef92f6e4856a688a2c5ce548a4872fd4
LukeBarrick <61234214+LukeBarrick@users.noreply.github.com>
2026-06-17 01:02:00 +0100

    Update showcase.component.spec.ts

--- projects/uilibrary/src/lib/showcase/showcase.component.spec.ts
+++ projects/uilibrary/src/lib/showcase/showcase.component.spec.ts
@@ -1,9 +1,10 @@
 /* tslint:disable:no-unused-variable */
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockProvider } from 'ng-mocks';
 
 import { ShowcaseComponent } from './showcase.component';
+import { ToastService } from '../core/services/toast.service';
 
 describe('ShowcaseComponent', () => {
   let component: ShowcaseComponent;
@@ -11,7 +12,9 @@ describe('ShowcaseComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShowcaseComponent ]
+      declarations: [ ShowcaseComponent ],
+      imports: [ ReactiveFormsModule ],
+      providers: [ MockProvider(ToastService) ]
     })
     .compileComponents();
   }));
